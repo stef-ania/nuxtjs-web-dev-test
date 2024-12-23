d1251c9b583e06410d8352b6443384d169a59595
stef-ania <stebofof@gmail.com>
2024-12-23 11:13:10 +0100

    feat: add hover effect in BookmarkBtn

--- components/ui/BookmarkBtn.vue
+++ components/ui/BookmarkBtn.vue
@@ -1,6 +1,18 @@
 <template>
   <button class="bookmark-btn">
-    <img src="/assets/img/icons/bookmark.svg" alt="Bookmark" />
+    <svg
+      class="bookmark-icon"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.7094 15.6388L5.5 20.0741V3.54565H18.5V20.0741L12.2906 15.6388L12 15.4312L11.7094 15.6388Z"
+        stroke="white"
+      />
+    </svg>
   </button>
 </template>
 
@@ -12,8 +24,17 @@
   transition: transform 0.2s ease;
 }
 
-.bookmark-btn img {
+.bookmark-icon {
   width: 24px;
   height: 24px;
+  transition: transform 0.3s ease, fill 0.3s ease, fill-opacity 0.3s ease;
+  fill-opacity: 0.4;
+  fill: #fff;
+}
+
+.bookmark-icon:hover {
+  transform: scale(1.1);
+  fill-opacity: 1;
+  fill: var(--color-primary-500);
 }
 </style>
